ee78958bf0636d7b2755bf00cce39e1100ab3416
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-04-09 23:00:16 +0200

    ss

--- src/css/js/pixabay-api.js
+++ src/css/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function fetchGallery(query, page) {
-  const BASE_URL = 'https:/pixabay.com';
+  const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api';
   const KEY_API = '?key=32952239-1d47564a6cdd6d985205bb869';
   const PARAMS = `&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`;
